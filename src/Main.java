4d13c787a82f423b1b71e94ddcee028b99a0fc74
Aviv Gueta <agueta@mycwt.com>
2020-04-23 15:45:58 +0300

    added Tree class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+
+
+
+
+    public static int countTrees(List<Tree> forest, double angle){
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        List<Tree> forest = new ArrayList<Tree>();
+        double angle = 10.0;
+
+        System.out.println(countTrees(forest, angle));
     }
 }
