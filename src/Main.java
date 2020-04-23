c28402274d7dcd1b3c0cd859f814b311266e1b97
Aviv Gueta <agueta@mycwt.com>
2020-04-23 16:04:52 +0300

    implement comparable and sorted the forest

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -8,9 +9,13 @@ public class Main {
 
 
     public static int countTrees(List<Tree> forest, double angle){
-
-
-
+        //calculate the degree from center to each tree
+        for (Tree t : forest){
+            t.calculateAndSetDegree(0,0);
+        }
+        //sort the forest by degree
+        Collections.sort(forest);
+        
 
         return 0;
     }
@@ -19,7 +24,9 @@ public class Main {
 
         List<Tree> forest = new ArrayList<Tree>();
         double angle = 10.0;
-
+        Tree tree1 = new Tree(40,50);
+        tree1.calculateAndSetDegree(0,0);
+        System.out.println(tree1.getDegreeFromCenter());
         System.out.println(countTrees(forest, angle));
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,4 +1,4 @@
-public class Tree {
+public class Tree implements Comparable<Tree>{
     private int x;
     private int y;
     private double degreeFromCenter;
@@ -27,6 +27,13 @@ public class Tree {
         this.y = y;
     }
 
+    public double getDegreeFromCenter() {
+        return degreeFromCenter;
+    }
+
+    public void setDegreeFromCenter(double degreeFromCenter) {
+        this.degreeFromCenter = degreeFromCenter;
+    }
 
     public void calculateAndSetDegree(int x, int y){
         double angle = Math.toDegrees(Math.atan2(y - this.y, x - this.x));
@@ -34,5 +41,15 @@ public class Tree {
             angle += 360;
         }
         this.degreeFromCenter = angle;
+    }
+
+    @Override
+    public int compareTo(Tree o) {
+        if(this.degreeFromCenter<o.degreeFromCenter){
+            return -1;
+        }else if(o.degreeFromCenter<this.degreeFromCenter) {
+            return 1;
+        }
+        return 0;
     }
 }
