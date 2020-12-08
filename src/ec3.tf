5b2dab3be87ddc707cabfdef9451a37c013508fa
avivgu <guetaaviv@gmail.com>
2020-12-08 15:51:16 +0200

    Update ec3.tf

--- src/ec3.tf
+++ src/ec3.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web_host" {
   ami           = "${var.ami}"
   instance_type = "t2.nano"
 
+  
   vpc_security_group_ids = [
     "${aws_security_group.web-node.id}"]
   subnet_id = "${aws_subnet.web_subnet.id}"
@@ -25,12 +26,11 @@ EOF
 resource "aws_ebs_volume" "web_host_storage" {
   # unencrypted volume
   availability_zone = "${var.availability_zone}"
-  #encrypted         = false  # Setting this causes the volume to be recreated on apply
+  encrypted         = false  # Setting this causes the volume to be recreated on apply
   size = 1
   tags = {
     Name = "${local.resource_prefix.value}-ebs"
   }
-  encrypted = true
 }
 
 resource "aws_ebs_snapshot" "example_snapshot" {
