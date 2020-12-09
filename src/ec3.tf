51cf23d87e55be2a5a384fdb8d314c0463f27110
avivgu <guetaaviv@gmail.com>
2020-12-09 14:20:56 +0200

    Update ec3.tf

--- src/ec3.tf
+++ src/ec3.tf
@@ -23,9 +23,7 @@ EOF
 }
 
 resource "aws_ebs_volume" "web_host_storage" {
-  # unencrypted volume
   availability_zone = "${var.availability_zone}"
-  #encrypted         = false  # Setting this causes the volume to be recreated on apply
   size = 1
   tags = {
     Name = "${local.resource_prefix.value}-ebs"
