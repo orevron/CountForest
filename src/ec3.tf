23dbd3e185ad8cd1d349615511cfde20e328d164
avivgu <guetaaviv@gmail.com>
2021-02-22 17:43:09 +0200

    Update ec3.tf

--- src/ec3.tf
+++ src/ec3.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web_host" {
   ami           = "${var.ami}"
   instance_type = "t2.nano"
 
+  
   vpc_security_group_ids = [
     "${aws_security_group.web-node.id}"]
   subnet_id = "${aws_subnet.web_subnet.id}"
@@ -35,6 +36,7 @@ resource "aws_ebs_snapshot" "example_snapshot" {
   volume_id   = "${aws_ebs_volume.web_host_storage.id}"
   description = "${local.resource_prefix.value}-ebs-snapshot"
   tags = {
+    
     Name = "${local.resource_prefix.value}-ebs-snapshot"
   }
 }
@@ -43,6 +45,7 @@ resource "aws_security_group" "web-node" {
   # security group is open to the world in SSH port
   name        = "${local.resource_prefix.value}-sg"
   description = "${local.resource_prefix.value} Security Group"
+  
   vpc_id      = aws_vpc.web_vpc.id
 
   ingress {
@@ -73,6 +76,7 @@ resource "aws_vpc" "web_vpc" {
   cidr_block           = "172.16.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
+  
   tags = {
     Name = "${local.resource_prefix.value}-vpc"
   }
@@ -82,6 +86,7 @@ resource "aws_subnet" "web_subnet" {
   vpc_id                  = aws_vpc.web_vpc.id
   cidr_block              = "172.16.10.0/24"
   availability_zone       = var.availability_zone
+  
   map_public_ip_on_launch = true
 
   tags = {
@@ -93,6 +98,7 @@ resource "aws_subnet" "web_subnet2" {
   vpc_id                  = aws_vpc.web_vpc.id
   cidr_block              = "172.16.11.0/24"
   availability_zone       = var.availability_zone2
+  
   map_public_ip_on_launch = true
 
   tags = {
@@ -106,12 +112,12 @@ resource "aws_internet_gateway" "web_igw" {
 
   tags = {
     Name = "${local.resource_prefix.value}-igw"
+    
   }
 }
 
 resource "aws_route_table" "web_rtb" {
   vpc_id = aws_vpc.web_vpc.id
-
   tags = {
     Name = "${local.resource_prefix.value}-rtb"
   }
@@ -120,16 +126,19 @@ resource "aws_route_table" "web_rtb" {
 resource "aws_route_table_association" "rtbassoc" {
   subnet_id      = aws_subnet.web_subnet.id
   route_table_id = aws_route_table.web_rtb.id
+  
 }
 
 resource "aws_route_table_association" "rtbassoc2" {
   subnet_id      = aws_subnet.web_subnet2.id
+  
   route_table_id = aws_route_table.web_rtb.id
 }
 
 resource "aws_route" "public_internet_gateway" {
   route_table_id         = aws_route_table.web_rtb.id
   destination_cidr_block = "0.0.0.0/0"
+  
   gateway_id             = aws_internet_gateway.web_igw.id
 
   timeouts {
@@ -140,6 +149,7 @@ resource "aws_route" "public_internet_gateway" {
 
 resource "aws_network_interface" "web-eni" {
   subnet_id   = aws_subnet.web_subnet.id
+  
   private_ips = ["172.16.10.100"]
 
   tags = {
@@ -151,6 +161,7 @@ resource "aws_network_interface" "web-eni" {
 resource "aws_flow_log" "vpcflowlogs" {
   log_destination      = aws_s3_bucket.flowbucket.arn
   log_destination_type = "s3"
+  
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.web_vpc.id
 
@@ -163,7 +174,6 @@ resource "aws_flow_log" "vpcflowlogs" {
 resource "aws_s3_bucket" "flowbucket" {
   bucket        = "${local.resource_prefix.value}-flowlogs"
   force_destroy = true
-
   tags = {
     Name        = "${local.resource_prefix.value}-flowlogs"
     Environment = local.resource_prefix.value
@@ -173,6 +183,7 @@ resource "aws_s3_bucket" "flowbucket" {
 output "ec2_public_dns" {
   description = "Web Host Public DNS name"
   value       = aws_instance.web_host.public_dns
+  
 }
 
 output "vpc_id" {
@@ -182,10 +193,12 @@ output "vpc_id" {
 
 output "public_subnet" {
   description = "The ID of the Public subnet"
+  
   value       = aws_subnet.web_subnet.id
 }
 
 output "public_subnet2" {
   description = "The ID of the Public subnet"
   value       = aws_subnet.web_subnet2.id
+  
 }
