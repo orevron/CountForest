13e92553cbaf9c4dfac7efbc7fd4956b131e9f29
Or Evron <20145882+orevron@users.noreply.github.com>
2022-03-15 16:14:42 +0200

    first commit

---
+++ s4.tf
@@ -0,0 +1,19 @@
+resource "aws_sns_topic" "example1" {
+  kms_master_key_id = "value"
+  tags              = {
+    Environment = "dev"
+    Owner       = "apps-team"
+  }
+}
+
+resource "aws_sns_topic" "example2" {
+
+}
+
+resource "aws_sns_topic" "example3" {
+  kms_master_key_id = "value"
+  tags              = {
+    Environment = "dev"
+    Owner       = "apps-team"
+  }
+}
